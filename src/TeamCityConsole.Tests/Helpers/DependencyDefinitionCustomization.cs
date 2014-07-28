58247a85c3840b3a1e08018f9a43393f43aa41c1
Gabriel Perez <gperez@computerworkware.com>
2014-07-28 11:47:29 -0400

    Refactor folder structure.

---
+++ src/TeamCityConsole.Tests/Helpers/DependencyDefinitionCustomization.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Ploeh.AutoFixture;
+using TeamCityApi.Domain;
+
+namespace TeamCityConsole.Tests.Helpers
+{
+    public class DependencyDefinitionCustomization : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            fixture.Customize<DependencyDefinition>(
+                composer =>
+                    composer.With(x => x.Properties,
+                        new List<Property> {new Property {Name = "pathRules", Value = "file.dll => assemblies "}}));
+        }
+    }
+}
