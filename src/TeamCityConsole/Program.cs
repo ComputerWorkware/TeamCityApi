0beeebb7b75894f41495d809d00e04a28a76acfb
Alexander Puchkov <me@apuchkov.com>
2015-10-23 10:36:24 -0400

    Added LibGit2Sharp to TeamCityConsole and embed the git2 native assembly and extract at runtime.

--- src/TeamCityConsole/Program.cs
+++ src/TeamCityConsole/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -23,6 +25,8 @@ namespace TeamCityConsole
 
         static void Main(string[] args)
         {
+            ExtractResources();
+
             var container = SetupContainer();
 
             DisplayAssemblyInfo();
@@ -59,6 +63,38 @@ namespace TeamCityConsole
 #endif
 
             ExecuteAsync(command, options).GetAwaiter().GetResult();
+        }
+
+        private const string ExtractPrefix = ".extract.";
+        private static void ExtractResources()
+        {
+            string assemblyLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            var assem = Assembly.GetExecutingAssembly();
+
+            string resourceName = assem.GetManifestResourceNames().FirstOrDefault(rn => rn.Contains(ExtractPrefix));
+
+            int extractPosition = resourceName.IndexOf(ExtractPrefix, StringComparison.Ordinal);
+
+            var dllName = resourceName.Substring(extractPosition+ExtractPrefix.Length);
+            
+            string assemblyFullName = Path.Combine(assemblyLocation,dllName);
+
+            if (File.Exists(assemblyFullName))
+                return;
+
+            if (resourceName == null) return; 
+
+            using (var stream = assem.GetManifestResourceStream(resourceName))
+            {
+                Byte[] assemblyData = new Byte[stream.Length];
+
+                stream.Read(assemblyData, 0, assemblyData.Length);
+
+                File.WriteAllBytes(assemblyFullName,assemblyData);
+
+            }
+
         }
 
         private static async Task ExecuteAsync(ICommand command, dynamic options)
