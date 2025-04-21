9174210cb87f7745adb99b696893a8cea4e8062a
Mohammad Silavi <mohammad.silavi@greenshield.ca>
2025-04-21 13:23:40 -0400

    Add Initial Year to propagate version use case

--- src/TeamCityApi/UseCases/PropagateVersionUseCase.cs
+++ src/TeamCityApi/UseCases/PropagateVersionUseCase.cs
@@ -23,15 +23,16 @@ namespace TeamCityApi.UseCases
             var buildConfig = await _client.BuildConfigs.GetByConfigurationId(buildConfigId);
             var majorVersion = buildConfig.Parameters[ParameterName.MajorVersion].Value;
             var minorVersion = buildConfig.Parameters[ParameterName.MinorVersion].Value;
+            var initialYear = buildConfig.Parameters[ParameterName.InitialYear].Value;
             var buildConfigChain = new BuildConfigChain(_client.BuildConfigs, buildConfig);
-            await PropagateVersion(buildConfigChain, majorVersion, minorVersion, simulate);
+            await PropagateVersion(buildConfigChain, majorVersion, minorVersion, initialYear, simulate);
         }
 
-        private async Task PropagateVersion(BuildConfigChain buildConfigChain, string majorVersion, string minorVersion, bool simulate)
+        private async Task PropagateVersion(BuildConfigChain buildConfigChain, string majorVersion, string minorVersion, string initialYear, bool simulate)
         {
             foreach (var node in buildConfigChain.Nodes)
             {
-                Log.Info($"Setting {node.Value.Id} to version {majorVersion}.{minorVersion}");
+                Log.Info($"Setting {node.Value.Id} to version {majorVersion}.{minorVersion} (Initial Year: {initialYear})");
                 if (!simulate)
                 {
                     //1st pass: set different, then in a project, value. Just to make parameter "own", see more: https://youtrack.jetbrains.com/issue/TW-42811
@@ -61,6 +62,21 @@ namespace TeamCityApi.UseCases
                         l => l.WithId(node.Value.Id),
                         ParameterName.MinorVersion,
                         minorVersion,
+                        true
+                    );
+
+                    //1st pass: set different, then in a project, value. Just to make parameter "own", see more: https://youtrack.jetbrains.com/issue/TW-42811
+                    await _client.BuildConfigs.SetParameterValue(
+                        l => l.WithId(node.Value.Id),
+                        ParameterName.InitialYear,
+                        "Temporary value, different from the parent project value!"
+                    );
+
+                    //2ns pass: set real value.
+                    await _client.BuildConfigs.SetParameterValue(
+                        l => l.WithId(node.Value.Id),
+                        ParameterName.InitialYear,
+                        initialYear,
                         true
                     );
                 }
